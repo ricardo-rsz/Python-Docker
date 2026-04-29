458580cc8565517e4c279840fa89084bb95d7904
ricardo-rsz <ricardosanchezzaragoza5@gmail.com>
2026-04-29 12:27:15 +0000

    Usuario envia archivo

--- Proyecto-1/app.py
+++ Proyecto-1/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from crypto import encrypt, decrypt
+import base64
 
 app = Flask(__name__)
 
@@ -30,6 +31,22 @@ def decrypt_route():
     text = decrypt(cipher_text)
 
     return jsonify({"text": text})
+
+@app.route("/encrypt-file", methods=["POST"])
+def encrypt_file_route():
+    if "file" not in request.files:
+        return jsonify({"error": "Falta el archivo"}), 400
+    
+    file = request.files["file"]
+    content = file.read()
+
+    try:
+        text_content = content.decode("utf-8")
+    except UnicodeDecodeError:
+        text_content = base64.b64encode(content).decode("utf-8") 
+
+    cipher = encrypt(text_content)
+    return jsonify({"filename": file.filename, "cipher": cipher})
 
 if __name__=="__main__":
     app.run(debug=True)
